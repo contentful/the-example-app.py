cba3c92cfc25ffea8d1386f43c9eeefd6ff8828b
David Litvak <david.litvakb@gmail.com>
2017-12-14 13:17:46 +0100

    Add documentation for sanitize_datetime

--- lib/entry_state.py
+++ lib/entry_state.py
@@ -101,6 +101,13 @@ def has_pending_changes(preview_entry, delivery_entry):
 
 
 def sanitize_datetime(date):
+    """In order to have a more accurate comparison due to minimal delays
+    upon publishing entries. We strip milliseconds from the dates we compare.
+    
+    :param date: datetime.datetime
+    :return: datetime.datetime without milliseconds.
+    """
+
     return date.replace(microsecond=0)
 
 
