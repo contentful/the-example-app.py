8ead58f6eb3598f5dfb83c1b270fa14917dcf486
Robert Linde <robert@selberglinde.se>
2018-01-22 12:02:57 +0100

    Switch markdown rendering to commonmark to comply to web app standard

---
+++ lib/markdown.py
@@ -0,0 +1,16 @@
+import CommonMark
+from flask import Markup
+
+def markdown(text):
+    """Filter for turning text into markdown.
+
+    :param text: String to be transformed.
+    :return: Transformed html string.
+
+    Usage:
+
+        {{ 'Some *markdown*'|markdown }}
+        "Some <strong>markdown</strong>"
+    """
+    print(text)
+    return Markup(CommonMark.commonmark(text))
